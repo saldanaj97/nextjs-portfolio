c09c8446f740ff6c0d8c8a432332b443a7ffbe6e
Juan Saldana <saldanaj97@gmail.com>
2023-10-05 13:52:04 -0500

    linter and prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from 'tailwindcss'
 import { nextui } from '@nextui-org/react'
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
@@ -8,14 +8,14 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  plugins: [nextui()],
   theme: {
     extend: {
       backgroundImage: {
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [nextui()]}
+}
 export default config

c82870bb827c583b9f0fb9fea63326cf04185608
Juan Saldana <saldanaj97@gmail.com>
2023-10-05 13:38:52 -0500

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/react'
 
 const config: Config = {
   content: [
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,6 +17,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [nextui()]}
 export default config

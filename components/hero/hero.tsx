9d442cc27212f9d3d9e1fe2691729f55eccfe127
Juan Saldana <saldanaj97@gmail.com>
2023-10-09 13:44:25 -0500

    updated header img

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import emoji from 'react-easy-emoji'
 
-import heroImg from '../../app/assets/images/hero-img.svg'
+import heroImg from '../../app/assets/images/hero-img.jpg'
 import HeroButton from '../buttons/heroButton'
 import { greeting } from '../portfolioData'
 
@@ -11,7 +11,16 @@ export default function Hero() {
     <div id="greeting" className="px-12 md:h-screen">
       <div className="flex flex-col justify-center md:flex-row">
         <div className="md:flex md:w-1/2">
-          <Image alt="computer-image" width="550" height="550" src={heroImg} />
+          {/* Overlay */}
+          <Image
+            alt="computer-image"
+            width="550"
+            height="550"
+            src={heroImg}
+            className="absolute z-0 translate-y-1 scale-110 rounded-full object-fill opacity-50 blur-xl saturate-150 filter"
+          />
+          {/* Image */}
+          <Image alt="computer-image" width="550" height="550" src={heroImg} className="z-20 rounded-full" />
         </div>
         <div className="flex flex-col justify-center md:w-1/2 ">
           <div className="mb-4 flex flex-row text-xl">
